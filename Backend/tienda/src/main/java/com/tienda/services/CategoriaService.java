b72cc3b510961fdf71f4b6cb3bdae39a8e13fe3a
El Wicho <luis85cbe@gmail.com>
2020-11-06 16:50:44 -0600

    Se añadieron Querys en categorias

--- Backend/tienda/src/main/java/com/tienda/services/CategoriaService.java
+++ Backend/tienda/src/main/java/com/tienda/services/CategoriaService.java
@@ -8,5 +8,9 @@ import com.tienda.entities.CategoriaEntity;
 public interface CategoriaService {
 	
 	public List<CategoriaEntity> encontraCategorias();
+	
+	public List <CategoriaEntity> SuperCategorias();
+	
+	public List <CategoriaEntity> SubCategorias(Long id);
 
 }
